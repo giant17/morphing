d8d00e2d2b58d2acafc27f0a605dd069a0153f78
Gianluca <giantadori@gmail.com>
2019-10-22 10:05:28 +0200

    Add init file

---
+++ init.m
@@ -0,0 +1,170 @@
+% Face Morphing
+
+% TODO: Document stuff
+
+%% Define constants
+
+%TODO: pathFrom =
+%TODO: pathTo =
+pathShine = '~/.local/share/toolbox/shine';
+
+%% Shine images
+
+shineImages(pathFrom, pathTo, pathShine);
+
+%% Create sequence
+
+% Return list ids/emos
+
+p = '~/Data/faceemo';
+x = dir(p);
+x = {x(3:end).name};
+
+ids = {};
+emos = {};
+
+for iimg=1:length(x)
+    a = regexp(x{iimg}, '_', 'split');
+
+    if ~ismember(a{1}, ids)
+        ids{end+1} = a{1};
+    end
+    if ~ismember(a{2}, emos)
+        emos{end+1} = a{2};
+    end
+end
+
+ids
+emos
+
+% cat for concatenate
+
+% TODO: if into if for morphing not doubling
+
+frames = 300;
+
+i = 1;
+id = ids{randi(length(ids))};
+
+
+
+length(ids)
+emo = emos{randi(length(emos))};
+img = imread([p '/' id '_' emo '_039.bmp']);
+seq = [];
+i = 1;
+while size(seq,3) <= frames
+
+	% Only 30 frame
+	if (mod(i,30) == 0) && (mod(i,40) ~= 0)
+		disp(['here is the ' num2str(i) ' frame'])
+
+		% Change id
+		disp('Changing id..')
+		disp('iTrying first id..')
+		idNew = ids{randi(length(ids))};
+		id = ids{randi(length(ids))};
+		while true
+			if idNew == id
+				idNew = ids{randi(length(ids))};
+			else
+				id = idNew;
+				break
+			end
+		end
+
+		% New Image
+		newImgName = [p '/' id '_' emo '_039.bmp'];
+		newImage = imread(newImgName);
+
+		% Morph
+		morph = minPhaseInterp(img, newImage, linspace(.3,.7,9));
+		seq = cat(3,seq,morph);
+
+		% Add frames
+		i = i + 9;
+
+		% Old image
+		img = newImage;
+
+	% Only 40 frame
+	elseif (mod(i,40) == 0) && (mod(i,30) ~= 0)
+		disp(['here is the ' num2str(i) ' frame'])
+
+		% Change emotion
+		disp('Changing emo..')
+		emoNew = emos{randi(length(emos))};
+		while true
+			if emoNew == emo
+				emoNew = emos{randi(length(emos))};
+			else
+				emo = emoNew;
+				break
+			end
+		end
+
+		% New Image
+		newImgName = [p '/' id '_' emo '_039.bmp'];
+		newImage = imread(newImgName);
+
+		% Morph
+		morph = minPhaseInterp(img, newImage, linspace(.3,.7,9));
+		seq = cat(3,seq,morph);
+
+		% Add frames
+		i = i + 9;
+
+		% Old image
+		img = newImage;
+
+	% Both
+	elseif (mod(i,30) == 0) & (mod(i,30) == 0)
+		disp(['here is the ' num2str(i) ' frame'])
+
+		% Change id
+		disp('Changing id..')
+		idNew = ids{randi(length(ids))};
+		while true
+			if idNew == id
+				idNew = ids{randi(length(ids))};
+			else
+				id = idNew;
+				break
+			end
+		end
+
+		% Change emotion
+		disp('Changing emo..')
+		emoNew = emos{randi(length(emos))};
+		while true
+			if emoNew == emo
+				emoNew = emos{randi(length(emos))};
+			else
+				emo = emoNew;
+				break
+			end
+		end
+
+		% New Image
+		newImgName = [p '/' id '_' emo '_039.bmp'];
+		newImage = imread(newImgName);
+
+		% Morph
+		morph = minPhaseInterp(img, newImage, linspace(.3,.7,9));
+		seq = cat(3,seq,morph);
+
+		% Add frames
+		i = i + 9;
+
+		% Old image
+		img = newImage;
+	end
+
+
+	% Add image
+	seq = cat(3,seq,img);
+	i = i + 1;
+end
+
+
+
